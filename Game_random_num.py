966c9b1f57de0904b68ec464b718723c2190b985
Bloodsilver <81293146+Bloodsilver@users.noreply.github.com>
2021-03-30 20:40:30 +0300

    Add files via upload

---
+++ Game_random_num.py
@@ -0,0 +1,35 @@
+import numpy as np
+   
+        
+def score_game(game_core):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1,101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
+
+def game_core_v2(number):
+    
+    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 1
+    predict = np.random.randint(1,101)
+    '''Вводим переменные максимального и минимального угаданного числа
+    для сокращения диапазона'''
+    a_max = ()
+    a_min = () 
+    while number != predict:
+        count+=1
+        if number > predict:
+            a_min = predict
+            predict = np.random.randint(a_min,101) ##
+           
+        elif number < predict: 
+            a_max = predict
+            predict = np.random.randint(a_min,a_max)
+    return(count) # выход из цикла, если угадали
+score_game(game_core_v2)
